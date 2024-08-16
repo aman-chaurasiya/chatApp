e39b964d259487d48d10568447e5ec5f3cc5787b
Aman Chaurasiya <amanchaurasiya786@gmail.com>
2024-08-16 19:43:12 +0530

    frontend done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      lets make chat apps
+      <button class="btn rounded-none px-16">Two</button>
+      <button class="btn btn-secondary">Two</button>
+    </div>
+  );
+}
+
+export default App;
